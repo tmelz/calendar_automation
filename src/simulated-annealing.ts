1e9f31767a40664669c78e3966494674bb051a42
Timothy Mellor <timothy.mellor@gmail.com>
2024-08-19 23:55:25 -0700

    SA stencil

---
+++ src/simulated-annealing.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace SimulatedAnnealing {
+  // set initial temp
+  // calculate initial cal cost
+  // for steps
+  //   increase temp
+  //   pick random calendar neighbor
+  //   calculate neighor cost
+  //   if accept(neighbor cost, current cost, temp)
+  //    accept neighbor as current
+}
